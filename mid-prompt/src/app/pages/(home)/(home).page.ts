9aab49bd87a7ea4a464fbe003655b68feffafc82
wanoo21 <ionprodan33@gmail.com>
2023-08-18 12:54:51 +0300

    refactor: small code refactor & first page

---
+++ mid-prompt/src/app/pages/(home)/(home).page.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouteMeta } from "@analogjs/router";
+
+export const routeMeta: RouteMeta = {
+  title: "Midjourney Prompt Generator",
+  meta: [
+    { name: "description", content: "Midjourney prompt generator" }
+  ]
+};
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./(home).page.html",
+  styleUrls: ["./(home).page.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class HomePage {
+}
